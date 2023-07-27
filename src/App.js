cda322270aa02a7352fe9d27be0f59515d471f54
TAIJULAMAN <taijulaman8@gmail.com>
2023-07-27 20:55:52 +0600

    using bootstrap by installing in terminal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { Button, Spinner } from 'react-bootstrap';
 import './App.css';
-
+import Cardgroup from './Component/CardGroup/Cardgroup';
+// import CardGroup from 'react-bootstrap/CardGroup';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>this is cards</h2>
+      <Cardgroup></Cardgroup>
+      <Button variant="primary">hi!!!!!!!!!!!!!!!</Button>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Spinner animation="border" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>
     </div>
   );
 }
